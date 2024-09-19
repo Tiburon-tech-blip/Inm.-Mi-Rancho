4218120590f3be0691a6c900edc091ed0b94b915
Alexis <130183158+Tiburon-tech-blip@users.noreply.github.com>
2024-09-18 22:56:31 -0300

    Add files via upload

--- assets/js/tours.js
+++ assets/js/tours.js
@@ -1,5 +1,40 @@
 
-let nombre =  prompt("Ingresa Tu Nombre") 
-let edad = parseInt(prompt("Ingresa Tu Edad"))
+//clase 07
 
-alert("Hola "+ nombre + " de " + edad +" años, te interesaria adquirir tickets")
+// let nombre =  prompt("Ingresa Tu Nombre") 
+// let edad = parseInt(prompt("Ingresa Tu Edad"))
+// alert("Hola "+ nombre + " de " + edad +" años, te interesaria adquirir tickets")
+
+//*******************************************************
+
+
+//clase 08
+
+// let nombre = prompt("Cuál es tu nombre?").toUpperCase();
+// const p = document.querySelector("#welcome");
+
+// if (nombre =="") {
+//     alert("Debes completar un nombre");
+//     nombre= prompt("Ingresa de Nuevo el nombre").toUpperCase();
+// }
+//     else if (nombre.length < 2) {
+//     nombre = prompt("Demasiado corto, dinos, cuál es realmente tu nombre?").toUpperCase();
+// }
+
+// p.textContent = "Hola, " + nombre;
+
+//*********************************************************
+
+//clase 09
+
+let nombre = prompt("Cuál es tu nombre?").toUpperCase();
+while (nombre.length < 3) {
+    nombre = prompt(
+        "Mmmm, tu nombre no puede ser tan corto, ingresa al menos 3 letras",
+    ).toUpperCase();
+}
+
+const span = document.getElementById("welcome");
+span.textContent = `Hola, ${nombre}`;
+const i = document.querySelector("i");
+i.setAttribute("class", "fa fa-ticket");
